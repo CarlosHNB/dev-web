49d4ed7832ea33257ae5ef0818aea37de991e542
siriuz <carloshnb.um@gmail.com>
2023-04-14 19:50:28 -0300

    Mudança do javascript

--- REVISOES/attr_perso/js/script.js
+++ REVISOES/attr_perso/js/script.js
@@ -1,13 +1,14 @@
-let lampada_estado = document.getElementById("lampada_apagada");
-let button_on = document.getElementById("ligar")
-let button_off = document.getElementById("desligar")
+const lampada_estado = document.getElementById("lampada_apagada");
+const buttons = document.getElementById("buttons");
 
 
-button_on.addEventListener("click", function(){
+console.log(buttons);
+
+/* button_on.addEventListener("click", function(){
     lampada_estado.setAttribute("src","./images/lampada-on.gif");
 })
 
 button_off.addEventListener("click", function(){
     lampada_estado.setAttribute("src", "./images/lampada-off.gif");
-})
+}) */
 
