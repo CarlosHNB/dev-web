7ba4f1b6e0f6a3c978ff0aaf30d245f86fc0d6fe
unknown <carloshnb.um@gmail.com>
2023-04-27 10:30:59 -0300

    Aula10 - Valor vs Referência

---
+++ js_avancado/aula10/script.js
@@ -0,0 +1,26 @@
+
+
+// function novoAluno(aluno, idade){
+//     return {aluno, idade};
+// }
+
+// let alunos = [
+//     novoAluno("Carlos", 23),
+//     novoAluno("Siriuz", 20),
+//     novoAluno("Die", 35),
+//     novoAluno("Bl4nk", 45),
+// ];
+
+// function alunoDaquiA5Anos(aluno){
+//     let novoAluno = aluno;
+//     novoAluno.idade +=5;
+//     return novoAluno;
+// }
+
+// console.log(alunos.map(alunoDaquiA5Anos));
+
+
+let idadeA = 15;
+let idadeB = idadeA;
+
+idadeB = 10;
