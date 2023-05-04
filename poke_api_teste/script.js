2de3878ca6d7e958aa2128d4f1c776518b5a39db
unknown <carloshnb.um@gmail.com>
2023-05-04 16:33:10 -0300

    Projeto PokeAPI

--- poke_api_teste/script.js
+++ poke_api_teste/script.js
@@ -1,12 +1,47 @@
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
 
-let pokemon_name = document.getElementById("pokemon");
-let button_search = document.getElementById("btn");
+const pokemon_name = document.getElementById("pokemon");
+const button_search = document.getElementById("btn");
+const informacoes = document.getElementById("informacoes");
 
+let pokemon = {}
+
+/* Função responsável por pegar as informações do pokemon */
+function getInfo(name_poke, id_poke, type_poke){
+
+  setInfo(name_poke, id_poke, type_poke);
+}
+
+/* Função responsável por setar as informações dentro do objeto pokemon */
+function setInfo(name_poke, id_poke, type_poke){
+
+    pokemon = {
+    nome: name_poke,
+    id: id_poke,
+    type: type_poke,
+
+  };
+
+  sendHTML();
+}
+
+/* Função responsável por enviar as informações para o front-end */
+function sendHTML(){
+  informacoes.innerHTML = 
+  `<p>Nome: ${pokemon.nome}</p>
+   <p>ID: ${pokemon.id}</p>
+   <p>Tipo: ${pokemon.type[0]["type"]["name"]}</p>
+  `
+}
+
+/* Fetch da API */
 button_search.addEventListener('click', () =>{
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name.value}`)
+   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name.value}`)
   .then(response => response.json())
-  .then(data => console.log(data.name))
+  .then(data => getInfo(data.name, data.id, data.types))
   .catch(err => console.log(err));
+
+
 });
+
